1f6ce100f57f59b1b6537691d7dbb1a5d51b774d
Daniel Demidko <demidko@farpost.com>
2022-12-06 10:30:59 +0500

    part two done

--- src/main/kotlin/com/adventofcode/Day.kt
+++ src/main/kotlin/com/adventofcode/Day.kt
@@ -1,21 +1,16 @@
 package com.adventofcode
 
-import com.adventofcode.Day.process
-import com.adventofcode.Day.solution
-
-object Day {
-
-  var solution = 0L; private set
-
-  fun process(line: String) {
-    
-  }
+fun solve(line: String): Int {
+  val codeSize = 14
+  val code = line.windowed(codeSize, 1).map(String::toSet).first { it.size == codeSize}.joinToString("")
+  return line.indexOf(code) + codeSize
 }
 
 fun main() {
   ::main.javaClass
     .getResourceAsStream("/input")!!
     .bufferedReader()
-    .forEachLine(::process)
-  println(solution)
+    .readText()
+    .let(::solve)
+    .let(::println)
 }
